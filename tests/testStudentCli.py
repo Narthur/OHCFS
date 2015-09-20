ce0cc8b1c460f41fd114e1b7d51abb6ed0d935fb
Nathan Arthur <narthur.a@gmail.com>
2015-09-20 08:36:20 -0500

    Added StudentCli class

--- tests/testStudentCli.py
+++ tests/testStudentCli.py
@@ -1,6 +1,6 @@
 import unittest
-
+from classes import StudentCli
 
 class TestStudentCli(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def testExists(self):
+        StudentCli.StudentCli()
