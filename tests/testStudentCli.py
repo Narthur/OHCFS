4631d312508555bbfd77b2989ee9d8d151db94c2
Nathan Arthur <narthur.a@gmail.com>
2015-09-22 09:03:09 -0500

    Refactored StudentCLI tests

--- tests/testStudentCli.py
+++ tests/testStudentCli.py
@@ -4,16 +4,18 @@ from mock import Mock, ReturnValues
 
 
 class TestStudentCli(unittest.TestCase):
-    def setUp(self):
-        self.mockedDatabase = Mock()
-
     def _initWithAddSubCommand(self):
+        self.mockedDatabase = Mock()
         self.studentCli = StudentCli.StudentCli(self.mockedDatabase,'student','add',['john doe'])
+
+    def _initWithConvertSubCommand(self):
+        self.mockedDatabase = Mock()
+        self.studentCli = StudentCli.StudentCli(self.mockedDatabase, 'leader', 'convert', ['john doe'])
 
     def testAddsStudent(self):
         self._initWithAddSubCommand()
         self.mockedDatabase.mockCheckCall(0,'addStudent','john','doe')
 
     def testGetsAllStudentsWhenConverting(self):
-        self.studentCli = StudentCli.StudentCli(self.mockedDatabase,'leader','convert',['john doe'])
+        self._initWithConvertSubCommand()
         self.mockedDatabase.mockCheckCall(0,'getAllStudents')
