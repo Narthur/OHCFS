c0dd812be4021573be1c84d5b39cc756bd72faf6
Nathan Arthur <narthur.a@gmail.com>
2015-09-23 08:54:11 -0500

    Refactored StudentCli

--- classes/StudentCli.py
+++ classes/StudentCli.py
@@ -8,13 +8,21 @@ class StudentCli:
         if command == 'student':
             self.processStudentCommands(filters)
         else:
-            students = self.db.getStudentsFromFilters(filters)
-            self.terminal.output('Selection:')
-            for student in students:
-                self.db.convertStudentToLeader(student[1],student[2])
+            self.processLeaderCommands(filters)
 
+    def processLeaderCommands(self, filters):
+        self.convertStudentsToLeaders(filters)
+
+    def convertStudentsToLeaders(self, filters):
+        students = self.db.getStudentsFromFilters(filters)
+        self.terminal.output('Selection:')
+        for student in students:
+            self.db.convertStudentToLeader(student[1], student[2])
 
     def processStudentCommands(self, filters):
+        self.addStudents(filters)
+
+    def addStudents(self, filters):
         for filter in filters:
             names = filter.split(' ')
             self.db.addStudent(names[0], names[1])
