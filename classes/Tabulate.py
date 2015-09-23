1e0d40526fca1ba271814c787acf7fb0a74d2c62
Nathan Arthur <narthur.a@gmail.com>
2015-09-23 08:45:04 -0500

    Imported Tabulate code from Friends3

---
+++ classes/Tabulate.py
@@ -0,0 +1,5 @@
+from tabulate import tabulate
+
+class Tabulate():
+    def tabulate(self, *args, **kwargs):
+        return tabulate(*args, **kwargs)
---
+++ tests/testTabulate.py
@@ -0,0 +1,12 @@
+import unittest
+from classes import Tabulate
+
+class TestTabulate(unittest.TestCase):
+    def setUp(self):
+        self.tabulate = Tabulate.Tabulate()
+
+    def testTableClassExists(self):
+        self.assertIsInstance(self.tabulate, Tabulate.Tabulate)
+
+    def testHasTabulateMethod(self):
+        self.assertTrue(hasattr(self.tabulate, 'tabulate'))
