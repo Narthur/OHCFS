c0707611206b1f37dd1ce8dd53590dab836fa117
Nathan Arthur <narthur.a@gmail.com>
2015-09-28 08:13:13 -0500

    Made runTests.py executable in terminal

--- runTests.py
+++ runTests.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+
+# Run in terminal by executing `./runTests.py`
+# If won't execute in Terminal, execute `chmod +x runTests.py` first.
+
 import unittest
 
 loader = unittest.TestLoader()
