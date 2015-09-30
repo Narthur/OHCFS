8d932601c6a62a343cc96edafdff943558ea594d
Nathan Arthur <narthur.a@gmail.com>
2015-09-30 07:50:08 -0500

    Removed superfluous HtmlGenerator test

--- tests/testHtmlGenerator.py
+++ tests/testHtmlGenerator.py
@@ -16,9 +16,6 @@ class TestHtmlGenerator(unittest.TestCase):
         expected = '<ul><li>1</li><li>2</li><li>3</li></ul>'
         self.assertEqual(list,expected)
 
-    def testLinkMethodTakesUrlAndText(self):
-        self.htmlGenerator.link('google.com','Google')
-
     def testLinkMethodReturnsLink(self):
         link = self.htmlGenerator.link('google.com','Google')
         expected = '<a href="google.com">Google</a>'
