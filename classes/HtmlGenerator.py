30cfc51f41125e66eb33a40238d6485ba832d927
Nathan Arthur <narthur.a@gmail.com>
2015-09-30 07:22:56 -0500

    Added list method to HtmlGenerator

--- classes/HtmlGenerator.py
+++ classes/HtmlGenerator.py
@@ -1,3 +1,15 @@
 class HtmlGenerator:
-    def h1(self, text):
-        return '<h1>{}</h1>'.format(text)
+    def _element(self, name, content):
+        return '<{}>{}</{}>'.format(name,content,name)
+
+    def h1(self, content):
+        return self._element('h1',content)
+
+    def _li(self, content):
+        return self._element('li',content)
+
+    def list(self, items):
+        itemMarkup = ''
+        for item in items:
+            itemMarkup += self._li(item)
+        return self._element('ul',itemMarkup)
--- tests/testHtmlGenerator.py
+++ tests/testHtmlGenerator.py
@@ -10,3 +10,11 @@ class TestHtmlGenerator(unittest.TestCase):
         heading = self.htmlGenerator.h1('head')
         expected = '<h1>head</h1>'
         self.assertEqual(heading, expected)
+
+    def testListMethodTakesItems(self):
+        self.htmlGenerator.list([1,2,3])
+
+    def testListMethodReturnsList(self):
+        list = self.htmlGenerator.list([1,2,3])
+        expected = '<ul><li>1</li><li>2</li><li>3</li></ul>'
+        self.assertEqual(list,expected)
