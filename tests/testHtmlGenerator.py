d4bf7791849f439c37b349c60742aaa26b6c6ccb
Nathan Arthur <narthur.a@gmail.com>
2015-09-30 07:15:12 -0500

    Removed superfluous HtmlGenerator test

--- tests/testHtmlGenerator.py
+++ tests/testHtmlGenerator.py
@@ -6,9 +6,6 @@ class TestHtmlGenerator(unittest.TestCase):
     def setUp(self):
         self.htmlGenerator = HtmlGenerator.HtmlGenerator()
 
-    def testHasH1Method(self):
-        self.htmlGenerator.h1('head')
-
     def testH1MethodMakesHeading(self):
         heading = self.htmlGenerator.h1('head')
         expected = '<h1>head</h1>'
