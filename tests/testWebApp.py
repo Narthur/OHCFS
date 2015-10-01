8474f9161af4bb11c3b919b7f7773902fcc266fc
Nathan Arthur <narthur.a@gmail.com>
2015-10-01 07:04:36 -0500

    Refactored testWebApp

--- tests/testWebApp.py
+++ tests/testWebApp.py
@@ -7,40 +7,39 @@ class TestWebApp(unittest.TestCase):
     def setUp(self):
         self.mockFieldStorage = Mock()
         self.mockHtmlGenerator = Mock({'h1':'heading','link':'link','list':'list'})
-        self.webApp = WebApp.WebApp(self.mockHtmlGenerator)
+        self.mockedWebApp = WebApp.WebApp(self.mockHtmlGenerator)
 
     def _assertContains(self, needle, haystack):
         contains = haystack.count(needle) > 0
         self.assertTrue(contains)
 
+    def _mockedOutput(self):
+        return self.mockedWebApp.getOutput(self.mockFieldStorage)
+
     def testOutputsHeading(self):
-        output = self.webApp.getOutput(self.mockFieldStorage)
-        self._assertContains('heading',output)
+        self._assertContains('heading',self._mockedOutput())
 
     def testMakesCanvassersLink(self):
-        self.webApp.getOutput(self.mockFieldStorage)
+        self.mockedWebApp.getOutput(self.mockFieldStorage)
         self.mockHtmlGenerator.mockCheckCall(1,'link','app.py?function=canvassers', 'Manage Canvassers')
 
     def testMakesList(self):
-        self.webApp.getOutput(self.mockFieldStorage)
+        self.mockedWebApp.getOutput(self.mockFieldStorage)
         self.mockHtmlGenerator.mockCheckCall(2,'list',['link'])
 
     def testReturnsList(self):
-        output = self.webApp.getOutput(self.mockFieldStorage)
-        self._assertContains('list',output)
+        self._assertContains('list',self._mockedOutput())
 
     def testMarksNavLinkAsCurrent(self):
         fieldStorage = Mock({'getvalue':'canvassers'})
-        self.webApp.getOutput(fieldStorage)
+        self.mockedWebApp.getOutput(fieldStorage)
         self.mockHtmlGenerator.mockCheckCall(1,'link','app.py?function=canvassers','Manage Canvassers','current')
 
     def testIncludesHeader(self):
         header = '''Content-Type: text/html
                 '''
-        output = self.webApp.getOutput(self.mockFieldStorage)
-        self._assertContains(header,output)
+        self._assertContains(header,self._mockedOutput())
 
     def testIncludesCss(self):
         css = '<link rel="stylesheet" type="text/css" href="../style.css">'
-        output = self.webApp.getOutput(self.mockFieldStorage)
-        self._assertContains(css,output)
+        self._assertContains(css,self._mockedOutput())
