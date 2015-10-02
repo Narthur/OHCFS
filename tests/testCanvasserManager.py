10bd7a832ada38dcc3ae8b18bc2741d5c1c733fb
Nathan Arthur <narthur.a@gmail.com>
2015-10-02 17:17:04 -0500

    Refactored testCanvasserManager

--- tests/testCanvasserManager.py
+++ tests/testCanvasserManager.py
@@ -1,14 +1,14 @@
 import unittest
 from classes import CanvasserManager
-from mock import Mock, ReturnValues
+from mock import Mock  # ReturnValues
 
 
 class TestCanvasserManager(unittest.TestCase):
     def setUp(self):
-        self.mockStudents = [[0,'John','Doe',0],[1,'Jo','Doe',1]]
+        self.mockStudents = [[0, 'John', 'Doe', 0], [1, 'Jo', 'Doe', 1]]
         self.mockDb = Mock({
-            'getAllCanvassers':self.mockStudents,
-            'getColumnNames':['names']
+            'getAllCanvassers': self.mockStudents,
+            'getColumnNames': ['names']
         })
         self.mockedCanvasserManager = CanvasserManager.CanvasserManager(self.mockDb)
 
@@ -18,43 +18,43 @@ class TestCanvasserManager(unittest.TestCase):
     def testGetFilteredStudents(self):
         filters = ['John Doe']
         students = self.mockedCanvasserManager.getEveryoneFromFilters(filters)
-        self.assertEqual(students,[[0,'John','Doe',0]])
+        self.assertEqual(students, [[0, 'John', 'Doe', 0]])
 
     def testGetFilteredStudentsWithOnlyFirstName(self):
         filters = ['John']
         students = self.mockedCanvasserManager.getEveryoneFromFilters(filters)
-        self.assertEqual(students, [[0,'John','Doe',0]])
+        self.assertEqual(students, [[0, 'John', 'Doe', 0]])
 
     def testGetFilteredStudentsWithPartialName(self):
         filters = ['Joh']
         students = self.mockedCanvasserManager.getEveryoneFromFilters(filters)
-        self.assertEqual(students, [[0,'John','Doe',0]])
+        self.assertEqual(students, [[0, 'John', 'Doe', 0]])
 
     def testConvertStudentToLeaderConvertsStudentToLeader(self):
-        self.mockedCanvasserManager.markCanvasserAsLeader('John','Doe')
-        self.mockDb.mockCheckCall(0,'markCanvasserAsLeader','John','Doe')
+        self.mockedCanvasserManager.markCanvasserAsLeader('John', 'Doe')
+        self.mockDb.mockCheckCall(0, 'markCanvasserAsLeader', 'John', 'Doe')
 
     def testAddStudentAddsStudent(self):
-        self.mockedCanvasserManager.addStudent('John','Doe')
-        self.mockDb.mockCheckCall(0,'addStudent','John','Doe')
+        self.mockedCanvasserManager.addStudent('John', 'Doe')
+        self.mockDb.mockCheckCall(0, 'addStudent', 'John', 'Doe')
 
     def testGetFilteredStudentsWithEmptyFiltersListReturnsAllStudents(self):
         students = self.mockedCanvasserManager.getEveryoneFromFilters([])
-        self.assertEqual(students,self.mockStudents)
+        self.assertEqual(students, self.mockStudents)
 
     def testGetFilteredStudentsWithoutCaps(self):
         filters = ['john']
         students = self.mockedCanvasserManager.getEveryoneFromFilters(filters)
-        self.assertEqual(students, [[0,'John','Doe',0]])
+        self.assertEqual(students, [[0, 'John', 'Doe', 0]])
 
-    def testHasGetCanvassarAttributeNamesMethod(self):
+    def testHasGetCanvasserAttributeNamesMethod(self):
         self.mockedCanvasserManager.getCanvasserAttributeNames()
 
     def testGetCanvasserAttributeNamesGetsThem(self):
         self.mockedCanvasserManager.getCanvasserAttributeNames()
-        self.mockDb.mockCheckCall(0,'getColumnNames','student')
+        self.mockDb.mockCheckCall(0, 'getColumnNames', 'student')
 
     def testReturnsCanvasserAttributeNames(self):
         actual = self.mockedCanvasserManager.getCanvasserAttributeNames()
         expected = ['names']
-        self.assertEqual(actual,expected)
+        self.assertEqual(actual, expected)
