a3851bad5fc01736b8e71f03307400b6bd754567
Nathan Arthur <narthur.a@gmail.com>
2015-10-02 18:04:17 -0500

    Worked on testTkGui

--- tests/testTkGui.py
+++ tests/testTkGui.py
@@ -1,12 +1,12 @@
 import unittest
 from classes import TkGui
-from mock import Mock, ReturnValues
+from mock import Mock  # ReturnValues
 
 
-class TestGui(unittest.TestCase):
+class TestTkGui(unittest.TestCase):
     def PASSsetUp(self):
         self.mockTk = Mock()
         self.mockedGui = TkGui.TkGui(self.mockTk)
 
     def PASStestAddsTitle(self):
-        self.mockTk.mockCheckCall(0,'addLabel','OHCFS')
+        self.mockTk.mockCheckCall(0, 'addLabel', 'OHCFS')
