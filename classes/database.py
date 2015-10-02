de65112ea46637b2c5bd7c3ed9f43b513fd6ead5
Nathan Arthur <narthur.a@gmail.com>
2015-10-02 17:52:10 -0500

    Cleaned up database

--- classes/database.py
+++ classes/database.py
@@ -1,38 +1,6 @@
-import numpy
-
 class Database:
     def __init__(self, sqliteInterface):
         self.sqlite = sqliteInterface
-
-    def getStudent(self, studentId=0, firstName='', lastName=''):
-        if studentId != 0 and firstName == '' and lastName == '':
-            self.c.execute('SELECT * FROM student WHERE studentId=?', studentId)
-            return self.c.fetchone()
-        elif studentId == 0 and firstName != '' and lastName == '':
-            self.c.execute('SELECT * FROM student WHERE firstName=?', firstName)
-            return self.c.fetchone()
-        elif studentId == 0 and firstName == '' and lastName != '':
-            self.c.execute('SELECT * FROM student WHERE lastName=?', lastName)
-            return self.c.fetchone()
-        elif studentId != 0 and firstName != '' and lastName == '':
-            # Check SQL syntax
-            self.c.execute('SELECT * FROM student WHERE studentId=? AND firstName=?',
-                           studentId, firstName)
-            return self.c.fetchone()
-        elif studentId != 0 and firstName == '' and lastName != '':
-            self.c.execute('SELECT * FROM student WHERE studentId=? AND lastName=?',
-                           studentId, lastName)
-            return self.c.fetchone()
-        elif studentId == 0 and firstName != '' and lastName != '':
-            self.c.execute('SELECT * FROM student WHERE firstName=? AND lastName=?',
-                           firstName, lastName)
-            return self.c.fetchone()
-        elif studentId != 0 and firstName != '' and lastName != '':
-            self.c.execute('SELECT * FROM student WHERE studentId=? AND firstName=? AND lastName=?',
-                           studentId, firstName, lastName)
-            return self.c.fetchone()
-        elif studentId == 0 and firstName == '' and lastName == '':
-            return None
 
     def getAllCanvassers(self):
         result = self.sqlite.executeQuery('SELECT * FROM student')
@@ -41,15 +9,15 @@ class Database:
     def addCanvasser(self, firstName, lastName):
         firstName = firstName.capitalize()
         lastName = lastName.capitalize()
-        format = "INSERT INTO student (firstName, lastName, isLeader) VALUES ('{0}', '{1}', 0)"
-        query = format.format(firstName, lastName)
+        formatString = "INSERT INTO student (firstName, lastName, isLeader) VALUES ('{0}', '{1}', 0)"
+        query = formatString.format(firstName, lastName)
         self.sqlite.executeQuery(query)
 
     def markCanvasserAsLeader(self, firstName, lastName):
         firstName = firstName.capitalize()
         lastName = lastName.capitalize()
-        format = "UPDATE student SET isLeader=1 WHERE firstName='{0}' AND lastName='{1}'"
-        query = format.format(firstName, lastName)
+        formatString = "UPDATE student SET isLeader=1 WHERE firstName='{0}' AND lastName='{1}'"
+        query = formatString.format(firstName, lastName)
         self.sqlite.executeQuery(query)
 
     def getColumnNames(self, table):
