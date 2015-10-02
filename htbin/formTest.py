90ea8af5af8904dc3cdd46204e001a27bf67ce24
Nathan Arthur <narthur.a@gmail.com>
2015-10-02 17:59:11 -0500

    Cleaned up formTest

--- htbin/formTest.py
+++ htbin/formTest.py
@@ -3,7 +3,10 @@
 
 # If this page isn't working, try executing `chmod +x [FILE]` in terminal.
 
-import cgitb, cgi, os; cgitb.enable()
+import cgitb
+import cgi
+
+cgitb.enable()
 
 form = cgi.FieldStorage()
 
@@ -13,15 +16,15 @@ print
 print '<h2>Using Get</h2>'
 print "<form method=get><input name='myGet' /><input type=submit /></form>"
 
-input = form.getfirst('myGet',None)
+val = form.getfirst('myGet', None)
 
-if (input != None):
-    print input
+if val:
+    print val
 
 print '<h2>Using Post</h2>'
 print "<form method=post><input name='myPost' /><input type=submit /></form>"
 
-input = form.getvalue('myPost',None)
+val = form.getvalue('myPost', None)
 
-if (input != None):
-    print input
+if val:
+    print val
