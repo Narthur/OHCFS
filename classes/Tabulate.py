8f060bf8df06b2132629fad5c06022ec648d50e3
Nathan Arthur <narthur.a@gmail.com>
2015-10-04 08:12:05 -0500

    Cleaned up Tabulate

--- classes/Tabulate.py
+++ classes/Tabulate.py
@@ -1,5 +1,10 @@
 from tabulate import tabulate
 
-class Tabulate():
-    def tabulate(self, *args, **kwargs):
+
+class Tabulate:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def tabulate(*args, **kwargs):
         return tabulate(*args, **kwargs)
